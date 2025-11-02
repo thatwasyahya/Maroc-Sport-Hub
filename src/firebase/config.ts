d3030713ecf44c7810953d85fafd25e6c9b40cbc
Yahya elyacoubi <yahyacoubi64@gmail.com>
2025-11-02 21:29:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7447088707-3956a",
+  "appId": "1:443305771721:web:e57fa080e687b413ae8318",
+  "apiKey": "AIzaSyC2n1WGYNwxUNPmdymBah4lcxRGXr52pvc",
+  "authDomain": "studio-7447088707-3956a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "443305771721"
+};
