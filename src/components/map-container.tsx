943ac37db8f43a4ca93131e36629a5d4726a8399
Yahya elyacoubi <yahyacoubi64@gmail.com>
2025-11-02 17:52:09 +0000

    Try fixing this error: `Runtime Error: Error: Map container is already i

--- src/components/map-container.tsx
+++ src/components/map-container.tsx
@@ -3,12 +3,11 @@
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet.markercluster';
 import { useEffect, useRef } from 'react';
 import type { Facility } from '@/lib/types';
-import Link from 'next/link';
 
 // Fix for default icon path issues with webpack
 const DefaultIcon = L.icon({
@@ -23,15 +22,14 @@ const DefaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
-const MarkerClusterGroup = ({ facilities }: { facilities: Facility[] }) => {
-  const map = useMap();
-
+const MarkerClusterGroup = ({ facilities, map }: { facilities: Facility[], map: L.Map | null }) => {
   useEffect(() => {
+    if (!map) return;
+
     const markerClusterGroup = L.markerClusterGroup();
 
     facilities.forEach(facility => {
       const marker = L.marker([facility.location.lat, facility.location.lng]);
-      // The popup content is now a simple div. We will use React to render into it.
       marker.bindPopup(`
         <div class="p-1">
           <h3 class="font-bold text-lg"><a href="/facilities/${facility.id}" class="hover:underline">${facility.name}</a></h3>
@@ -53,15 +51,22 @@ const MarkerClusterGroup = ({ facilities }: { facilities: Facility[] }) => {
 };
 
 const MapView = ({ facilities }: { facilities: Facility[] }) => {
+  const mapRef = useRef<L.Map | null>(null);
   const position: L.LatLngExpression = [33.5731, -7.5898]; // Default to Casablanca
 
   return (
-    <MapContainer center={position} zoom={7} scrollWheelZoom={true} className="h-full w-full z-0">
+    <MapContainer
+      center={position}
+      zoom={7}
+      scrollWheelZoom={true}
+      className="h-full w-full z-0"
+      whenCreated={map => { mapRef.current = map; }}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <MarkerClusterGroup facilities={facilities} />
+      <MarkerClusterGroup facilities={facilities} map={mapRef.current} />
     </MapContainer>
   );
 };
