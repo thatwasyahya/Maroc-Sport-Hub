79d79607134a10a2b01574e290037c091665a3db
Yahya elyacoubi <yahyacoubi64@gmail.com>
2025-11-10 13:02:23 +0000

    tiens cette image contient plusieurs tete tu peux l'utiliser pour les im

--- src/lib/avatars.ts
+++ src/lib/avatars.ts
@@ -1,7 +1,8 @@
 // A list of fun, cartoonish avatars for users to choose from.
-// Avatars provided by https://avatar.iran.liara.run/
+// Avatars provided by https://i.pravatar.cc/
+// This service provides random avatars, so we generate a list with different IDs.
 
-const avatarBaseUrl = 'https://avatar.iran.liara.run/public';
+const avatarBaseUrl = 'https://i.pravatar.cc/150?u=';
 
-// Generate a list of avatar URLs
-export const avatars: string[] = Array.from({ length: 30 }, (_, i) => `${avatarBaseUrl}/${i + 1}`);
+// Generate a list of avatar URLs with unique user IDs to get different images
+export const avatars: string[] = Array.from({ length: 30 }, (_, i) => `${avatarBaseUrl}user${i + 1}`);
