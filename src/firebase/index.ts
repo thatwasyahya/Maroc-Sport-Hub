5fcd2adff53c389bbde7a287b676226d095796eb
Yahya elyacoubi <yahyacoubi64@gmail.com>
2025-11-10 16:45:00 +0000

    j'ai l'impression que ça charge encore, ptetre il faut configurer quelqu

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,31 +3,17 @@
 
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
-import { getStorage, connectStorageEmulator } from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  const isEmulator = process.env.NEXT_PUBLIC_USE_EMULATOR === 'true';
-
   const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
   const firestore = getFirestore(app);
   const auth = getAuth(app);
   const storage = getStorage(app);
-
-  if (isEmulator) {
-    console.log('Using Firebase Emulators');
-    try {
-      connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
-      connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
-      connectStorageEmulator(storage, '127.0.0.1', 9199);
-    } catch (e) {
-      // Emulators are likely already connected
-      // console.warn(e);
-    }
-  }
 
   return { firebaseApp: app, auth, firestore, storage };
 }
