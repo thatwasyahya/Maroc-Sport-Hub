3cc6ae75998a4e6781b1ccc604c105541e145356
Yahya elyacoubi <yahyacoubi64@gmail.com>
2025-11-12 10:24:46 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,32 +1,28 @@
-'use client';
-
-import { useMemo } from 'react';
 import { notFound } from 'next/navigation';
 import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
 
-import Header from '@/components/header';
-import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase';
 import { FirebaseErrorListener } from '@/components/FirebaseErrorListener';
+import { Toaster } from '@/components/ui/toaster';
 
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
-export default function LocaleLayout({
+export default async function LocaleLayout({
   children,
   params: { locale },
 }: {
   children: React.ReactNode;
   params: { locale: string };
 }) {
-  const messages = useMemo(() => {
-    try {
-      return require(`../../messages/${locale}.json`);
-    } catch (error) {
-      notFound();
-    }
-  }, [locale]);
+  let messages;
+  try {
+    messages = await getMessages({ locale });
+  } catch (error) {
+    notFound();
+  }
 
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
