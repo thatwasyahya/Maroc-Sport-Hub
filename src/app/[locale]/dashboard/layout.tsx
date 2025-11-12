a80286976b1b8bbf9de0a22a246d706343d0403f
Yahya elyacoubi <yahyacoubi64@gmail.com>
2025-11-12 10:54:48 +0000

    /dashboard normal fonctionne correctement mais pas /fr/dashboard

--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -116,7 +116,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
                     href={href}
                     className={cn(
                       'flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary',
-                      pathname === href && 'bg-muted text-primary'
+                      pathname.endsWith(href) && 'bg-muted text-primary'
                     )}
                   >
                     <Icon className="h-4 w-4" />
