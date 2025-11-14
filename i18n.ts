a9b6c0f01d5c003b330b5e9d8a7468bb0f36c95b
Yahya elyacoubi <yahyacoubi64@gmail.com>
2025-11-14 20:00:20 +0000

    last

--- i18n.ts
+++ i18n.ts
@@ -1,10 +1,12 @@
-import {getRequestConfig} from 'next-intl/server';
- 
-export default getRequestConfig(async ({locale}) => {
-  // Validate that the incoming `locale` parameter is valid
-  // and load the translation files dynamically.
+import { getRequestConfig, requestLocale } from 'next-intl/server';
+
+export default getRequestConfig(async () => {
+  // Récupère la locale automatiquement depuis le contexte
+  const locale = await requestLocale();
+
+  // Charge dynamiquement les messages
   const messages = (await import(`./src/messages/${locale}.json`)).default;
- 
+
   return {
     messages,
     locale,
