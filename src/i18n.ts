6e3ab035479f402beddef3b04a3ab9776c06e82d
Yahya elyacoubi <yahyacoubi64@gmail.com>
2025-11-14 20:22:37 +0000

    21:20:00.537 Running build in Washington, D.C., USA (East) – iad1 21:20:

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,10 @@
-import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
  
-// Can be imported from a shared config
-const locales = ['en', 'fr', 'ar'];
- 
 export default getRequestConfig(async ({locale}) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
+  // Charge dynamiquement les messages pour la locale demandée.
+  const messages = (await import(`./messages/${locale}.json`)).default;
  
   return {
-    messages: (await import(`./messages/${locale}.json`)).default
+    messages
   };
 });
