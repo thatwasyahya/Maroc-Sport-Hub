4a2a46f449ebca8b05688bd24b3d633787541f87
Yahya elyacoubi <yahyacoubi64@gmail.com>
2025-11-14 20:25:57 +0000

    bon vercel ne veux pas du tout me laisser faire à cause de cette erreur

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,13 @@
 import {getRequestConfig} from 'next-intl/server';
  
 export default getRequestConfig(async ({locale}) => {
-  // Charge dynamiquement les messages pour la locale demandée.
-  const messages = (await import(`./messages/${locale}.json`)).default;
- 
-  return {
-    messages
-  };
+  // This is a workaround for a bug in next-intl with the app router
+  // see: https://github.com/amannn/next-intl/issues/165
+  try {
+    return {
+      messages: (await import(`./messages/${locale}.json`)).default
+    };
+  } catch (error) {
+    console.warn(`Could not load messages for locale ${locale}.`);
+  }
 });
